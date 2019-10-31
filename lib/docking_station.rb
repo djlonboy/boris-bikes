53de8fdfe1234ec22d7a602b2d47c99a961c05b0
djlonboy <djlonboy@gmail.com>
2019-10-31 15:56:00 +0000

    Added method for docking bikes, instance variable for storing them, and associated tests.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,18 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :bikes
+
+  def initialize
+     @bikes = []
+  end
+
   def release_bike
     Bike.new
   end
+
+  def dock_bike(bike)
+    @bikes.push(bike)
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,8 +5,23 @@ describe DockingStation do
   it {is_expected.to respond_to :release_bike}
 
   it 'gets a working bike' do
-
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  it {is_expected.to respond_to :dock_bike}
+
+  # check if att reader for @bikes returns a result
+
+  it 'has instance variable for storing bikes' do
+    expect(subject.bikes).to eq([])
+  end
+
+  it 'can save a docked bike' do
+    test_bike = Bike.new
+    docking_station = DockingStation.new
+    docking_station.dock_bike(test_bike)
+    expect(docking_station.bikes).to include(test_bike)
+  end
+
 end
