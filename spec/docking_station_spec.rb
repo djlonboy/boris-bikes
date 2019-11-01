eeee6df32d2cb960bb169e12204a3b398581d867
djlonboy <djlonboy@gmail.com>
2019-11-01 16:17:01 +0000

    Added constant for max capacity of bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -28,7 +28,7 @@ describe DockingStation do
   end
 
   it 'raises an error if there are too many bikes' do
-    20.times {subject.dock_bike(Bike.new)}
+    DockingStation::DEFAULT_CAPACITY.times {subject.dock_bike(Bike.new)}
     expect { subject.dock_bike Bike.new }.to raise_error("full")
   end
 
